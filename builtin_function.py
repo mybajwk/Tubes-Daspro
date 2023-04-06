b9da2092bbf29bd2041487d5b9a24b5bde3331d1
Enrique Yanuar <enriqueyanuar.7@gmail.com>
2023-04-06 21:38:29 +0700

    change import csv

--- builtin_function.py
+++ builtin_function.py
@@ -2,6 +2,7 @@ import main_data
 
 
 def len_data(data):
+    # pengganti len
     idx = 0
     for i in data:
         idx += 1
@@ -9,6 +10,7 @@ def len_data(data):
 
 
 def push_back_data(old_data, new_data):
+    # pengganti append
     idx = len_data(old_data)
     idx += 1
     data_result = [None for k in range(idx)]
@@ -19,5 +21,34 @@ def push_back_data(old_data, new_data):
     return data_result
 
 
-# tambahan fitur bonus
-# def random():
+def count_char(text, character):
+    # menghitung jumlah char yang dicari dalam suatu kata
+    idx = 0
+    for i in text:
+        if i == character:
+            idx += 1
+    return idx
+
+
+def csv_reader(data, delimiter):
+    result = []
+    for line in data:
+        data_line = []
+        last_word = False
+        idx = 0
+        word = ""
+        for character in line:
+            if character == "\n":
+                last_word = True
+            if character == delimiter:
+                data_line = push_back_data(data_line, word)
+                word = ""
+            else:
+                if not (last_word):
+                    word += character
+            idx += 1
+        data_line = push_back_data(data_line, word)
+        result = push_back_data(result, data_line)
+    return result
+    # tambahan fitur bonus
+    # def random():
--- load.py
+++ load.py
@@ -1,5 +1,6 @@
 import csv
 import main_data
+from builtin_function import csv_reader
 #  read csv ()
 
 
@@ -7,12 +8,12 @@ def read_file(file_name):
     match file_name:
         case "user":
             file = open(main_data.folder+"/"+file_name+".csv", 'r')
-            main_data.data_csv_user = list(csv.reader(file, delimiter=";"))
+            main_data.data_csv_user = csv_reader(file, ";")
         case "candi":
             file = open(main_data.folder+"/"+file_name+".csv", 'r')
-            main_data.data_csv_candi = list(csv.reader(file, delimiter=";"))
+            main_data.data_csv_candi = csv_reader(file, ";")
         case "bahan_bangunan":
             file = open(main_data.folder+"/"+file_name+".csv", 'r')
-            main_data.data_csv_bahan = list(csv.reader(file, delimiter=";"))
+            main_data.data_csv_bahan = csv_reader(file, ";")
         case _:
             print("File tidak ada yang sesuai")
