a268b9b2b36de567be90730be6c108c92df62f8d
Enrique Yanuar <enriqueyanuar.7@gmail.com>
2023-04-15 23:10:13 +0700

    hilangkan jinnnnnnnnn

--- function/hilangkan_jin.py
+++ function/hilangkan_jin.py
@@ -1,5 +1,16 @@
+import main_data
 
 
 def hilangkan_jin():
-    print()
-    # buat program disini hapus aja printnya
+    delete_user = input("masukkan usernama jin: ")
+    ditemukan = False
+    for i in range(1, main_data.user_len):
+        if main_data.data_csv_user[i][0] == delete_user:
+            ditemukan = True
+            confirm = input(
+                "Apakah anda yakin ingin menghapus jin dengan username Jin1 (Y/N)? ")
+            if confirm == "Y":
+                main_data.data_csv_user[i] = "\n"
+    if not ditemukan:
+        print("Tidak ada jin dengan username tersebut.")
+    print(main_data.data_csv_user)
