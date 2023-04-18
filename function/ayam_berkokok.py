59ffb9a00a82c4579b3e381291ba9493c1775e47
agiffachri11 <agiffachri11@gmail.com>
2023-04-18 16:35:50 +0700

    Update ayam_berkokok.py

--- function/ayam_berkokok.py
+++ function/ayam_berkokok.py
@@ -1,3 +1,14 @@
+
+
 def ayam_berkokok():
-    print()
-    # buat program disini hapus aja printnya
+    print("Kukuruyuk.. Kukuruyuk..")
+    jumlah_candi = 
+    print("Jumlah Candi: ",jumlah_candi)
+    if jumlah_candi >= 100 :
+        print("Yah, Bandung Bondowoso memenangkan permainan!")
+    else :
+        print("Selamat, Roro Jonggrang memenangkan permainan!")
+        print("*Bandung Bondowoso angry noise*")
+        print("Roro Jonggrang dikutuk menjadi candi.")
+    exit()
+    
