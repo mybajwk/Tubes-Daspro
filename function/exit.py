02a6a9063884ed2246d05df9cc89499e3e9f1ef3
Enrique Yanuar <enriqueyanuar.7@gmail.com>
2023-04-30 18:05:56 +0700

    Update exit.py

--- function/exit.py
+++ function/exit.py
@@ -9,7 +9,7 @@ def exit():
     while (opsi != 'y' and opsi != 'n' and opsi != 'Y' and opsi != 'N'):
         opsi = input(
             "Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
-    if opsi == 'y':
+    if opsi == 'y' or opsi == "Y":
         # lakukan saving data
         save()
     # tandai pada program untuk logout jika login dan stop program
